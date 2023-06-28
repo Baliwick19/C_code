4d59f417b852e87295bb15a76eafbaf81e5100d1
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-28 14:53:17 +0530

    integer_value_of_character

---
+++ main.c
@@ -0,0 +1,14 @@
+// p2_26 integer_value_of_character
+
+
+#include <stdio.h> 
+
+int main(){
+
+    int num ;
+
+    printf("Enter an Integer: ") ;
+    scanf("%d", &num ) ;
+
+    printf("%d = %c", num, num ) ;
+}
