2cea1da531e99a7d4dfd69920df69048db913f93
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-28 15:09:38 +0530

    c_statemnt

---
+++ main.c
@@ -0,0 +1,49 @@
+// p3_3 c_statement 
+
+
+#include <stdio.h>
+
+int main(){
+
+	// a)   
+	
+	int product = 3 ; 
+
+	product *= 2 ; 
+
+	//b)  
+	
+	product = product * 2 ;
+
+	// c)
+	
+	int count = 9 ; 
+
+	if ( count > 10 ){
+
+		printf("Count is greater than 10") ;
+
+	}
+
+	// d) 
+	
+	int quotient, divisor;
+
+	quotient = quotient % divisor ; 
+
+	//or 
+	
+	quotient %= divisor ; 
+
+	// e) 
+	
+	printf("%.2f", 123.4567) ; 
+    
+	// f) 
+	
+	printf("%.3f", 3.14159) ;
+	
+	return 0 ; 
+
+}
+
