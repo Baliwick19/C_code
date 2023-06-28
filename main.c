270ed879945b9566671054fdf1551a8ed1bf3640
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-28 14:52:03 +0530

    following_error

---
+++ main.c
@@ -0,0 +1,19 @@
+// p2_24 following_error
+
+/*
+
+Given the following code snippets, describe the type of the error, and explain what will happen when the program is executed. 
+
+a) int a = 2;
+   int b = a / 0;
+
+
+-> dividing buy zero will cause fatal error .
+
+b) int a = 2;
+   if (a = 3) { printf("a is 3"); }
+   else { printf("a is not 3"); } 
+
+-> assignment operation in if condition statement will always be true. Hence, it will always print "a is 3".
+
+*/
