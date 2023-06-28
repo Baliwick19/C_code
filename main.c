cd703595ccc0c19a9b3513a60d90710a9963a317
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-28 15:01:50 +0530

    fib

---
+++ main.c
@@ -0,0 +1,15 @@
+// p3_1 FIB 
+
+
+/*
+ 
+   a) Algorithm 
+   b) Program Control
+   c) Sequence, Selection and Iteration 
+   d) if-else
+   e) compund stataments or blocks 
+   f) while
+   g) definite
+   h) sentinel
+
+*/
