ac1bccffdf8d0a8a017bce25cf12ad03e2499652
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-28 14:46:51 +0530

    arithmetic_calculator

---
+++ main.c
@@ -0,0 +1,19 @@
+// arithmetic calculator 
+
+#include <stdio.h>
+
+int main(){
+
+    int number1, number2 ;
+
+    printf("Enter two integers: ") ; 
+    scanf("%d %d", &number1, &number2) ;
+
+    printf("Sum = %d\n", number1 + number2 );
+    printf("Product = %d\n", number1 * number2 ) ;
+    printf("Difference = %d\n", number1 - number2 ) ;
+    printf("Quotient = %d\n", number1 / number2 ) ;
+    printf("Remainder = %d\n", number1 % number2 ) ;   
+
+    return 0 ; 
+}
