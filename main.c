f5f9ddaedf4c088ac676721fc15807a8d39732d1
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-28 14:35:25 +0530

    excercise

---
+++ main.c
@@ -0,0 +1,44 @@
+// p2_3
+
+/*
+
+a)------------------------
+
+int number ;
+number = 0 ;
+
+or 
+
+int number = 0 ; 
+
+b)------------------------
+
+printf("%s", "Enter an integer: ") ;
+
+c)------------------------
+
+scanf("%d", &a) ;
+
+d)-----------------------
+
+if ( number != 7 ){
+    puts("number is not equal to 7.") ;
+}
+
+e)------------------------
+
+printf("This is a C program.") ; 
+
+f)------------------------
+
+printf("This is a C \nprogram.") ;  
+
+g)------------------------
+
+printf("This \nis \na C \nprogram.") ;
+
+h)------------------------
+
+printf("This \tis \ta C \tprogram.") ;
+
+*/
