d0dafc2df6308cd9a2a5a53b9b0b398cc28c9c24
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-28 15:23:33 +0530

    c_statement

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
+// p3_4 c_statement 
 
+
+#include <stdio.h>
+
+
+int main(){
+
+    // a)
+	
+	int a = 1 ;
+
+	// b)
+	
+	int sum = 0 ; 
+    
+	// c) 
+	
+	sum += x ;
+
+	// d) 
+	
+	printf("Sum is %d", sum) ; 
+
+	return 0 ; 
+
+}
