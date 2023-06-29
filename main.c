553d69dd2ab68bb804233df9f97842da23015bd0
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-29 21:29:46 +0530

    interest_calculator

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
 // p3_19  interest_calculator
+
+#include <stdio.h>
+
+int main(){
+	
+	double principal, rate, days ; 
+
+	printf("Enter loan principal ( -1 to end ): ");
+	scanf("%lf", &principal); 
+
+	while ( principal != -1 ) {
+		
+		printf("Enter interest rate: ");
+		scanf("%lf", &rate);
+
+		printf("Enter term of the loan in days: ");
+		scanf("%lf", &days);
+
+		printf("The interest charge is $%.2lf", principal * rate * days / 365 );
+
+		printf("\n\nEnter loan principal ( -1 to end ): ");
+		scanf("%lf", &principal); 
+
+	}
+
+	return 0 ;
+}
