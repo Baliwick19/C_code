73d7eb9dc3706a7635a6efffcf57b07356d9f35d
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-29 15:05:50 +0530

    errors

---
+++ main.c
@@ -0,0 +1,16 @@
+// p3_10 errors 
+//
+//
+// a) 
+// -> relation operator must be >= not => 
+// -> there should be a semicolon after first puts statement .
+//
+// b) 
+// -> semicolon after while keyword is an syntax error 
+// -> value of product will always be zero. 
+//
+// c) 
+// -> compound assignment operator must be += not =+ 
+// -> two statement should be enclosed with curly braces 
+//
+// d) infinite loop, y must be incremented. 
