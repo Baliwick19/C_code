e7a13fb370f421b729857d6475118fcefa809ea6
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-29 21:06:53 +0530

    sales_commission_calculator

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
 // p3_17  sales_commission_calculator
+
+#include <stdio.h>
+
+int main(){
+	
+	double sales ; 
+
+	printf("Enter sales in dollars ( -1 to end ): "); 
+	scanf("%lf", &sales);
+	
+	while ( sales != -1 ) {
+		
+		printf("Salary is: %.2lf", 200 + sales * 0.09 ); 
+		
+		printf("\nEnter sales in dollars ( -1 to end ): "); 
+		scanf("%lf", &sales);
+
+	}
+
+	return 0 ; 
+
+}
