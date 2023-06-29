ad18afad225981f7f7e7c9e14064672f532aa6b6
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-29 14:55:16 +0530

    errors

---
+++ main.c
@@ -0,0 +1,13 @@
+// p3_8 errors 
+
+// a) 
+ 
+/*
+
+a)  closing bracket is missing 
+
+b)  precision can be used in scanf conversion specifier 
+
+c) placing semicolon after else is a syntax error. 
+
+*/
