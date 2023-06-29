de9d63215a02238211fd6a5afb6dd7c143743430
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-29 14:59:19 +0530

    errors

---
+++ main.c
@@ -0,0 +1,12 @@
+// p3_9 errors
+
+
+
+// while ( z >= 0 ) {
+//		sum += z  ; 
+// }
+//
+//
+//
+// -> it would create an infinite loop, since z is always greater than zero. 
+// => for elemenating error we must decrease z by 1 in loop .
