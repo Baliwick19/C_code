072807456aea1abd9a80607507c3b263e89cb100
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-29 15:52:10 +0530

    pseudocode

---
+++ main.c
@@ -0,0 +1,13 @@
+// p3_12 pseudocode 
+//
+//
+// a) Print "Enter your name:."
+//
+// b) set p to product of variable a, b, c and d .
+//
+// c) if x is greater than y 
+//		 set x to 10 
+//	  else 
+//	     set x to 20 
+//
+// d) input value of   a, b, c and d.
