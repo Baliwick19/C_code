0ee55824f978a2c7ce46e644646d1264394e084c
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-29 15:17:42 +0530

    fib

---
+++ main.c
@@ -0,0 +1,10 @@
+// p3_11 FIB
+//
+// a) Sequential execution
+// b) Pseudocode 
+// c) sequence, selection and iteration 
+// d) rectangles, diamond, rounded rectangles and small circles.
+// e) Direction
+// f) switch
+// g) ?:, first, second, third 
+// h) selection 
