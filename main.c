a575bb10398ba4bba5bdc30bfd3da15fe5572752
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-06-30 10:26:47 +0530

    pre_post_incrementing

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
 // p3_21  pre_post_incrementing
+
+#include <stdio.h>
+
+int main(){
+	
+	// Preincrementing 
+	
+	int x = 5 ; 
+
+    // it first incremnets value of x by 1, then displays the values of x. 
+	printf("%d\n", ++x ); 
+	printf("%d\n", x ); 
+
+	// Postincrementing
+
+	x = 5 ; 
+
+	// it first displays the values of x then incremnets its value by 1.
+	printf("%d\n", x++ );
+	printf("%d\n", x );
+
+	return 0 ;
+}
