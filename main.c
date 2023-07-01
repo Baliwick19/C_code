951e02d73911b9dcad7b74cc79cc7112a89309e5
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-01 22:15:13 +0530

    square_of_asterisks

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
 /* p3_32 sqaure_of_asterisks */ 
+
+#include <stdio.h>
+
+int main(){
+	
+	printf("Enter a integer between 1 and 20 : ");
+	int size = 0 ; 
+	scanf("%d", &size);
+
+	int i = 1 ; 
+
+	while ( i <= size ) {
+		int j = 1 ; 
+		while ( j <= size ) {
+			printf("*");
+			j++ ; 
+		}
+		printf("\n");
+		i++ ; 
+	}
+
+	return 0 ;
+}
