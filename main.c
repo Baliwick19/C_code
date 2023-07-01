3b79731ef47d7a6da62d235172206e99163ff6d4
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-01 12:28:17 +0530

    tabular_oputput

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
 	
 	int n = 1  ; 
 	
-	printf("\n\nN \t N+3 \t N+6 \t N+9 \n\n");
+	printf("\n\nN \t N+3 \t N+6 \t N+9 \n\n") ;
 	while ( n <= 5 ) {
 		int m = 7 * n ; 
 		printf("%d \t %d \t %d \t %d\n", m, m + 3, m + 6, m + 9);
