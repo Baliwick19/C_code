2af89579fc638f86b78481af1fcc7e23a91545cc
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-01 11:49:08 +0530

    find_the_largest_number

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
 // p3_23  find_the_largest_number
+
+
+#include <stdio.h>
+
+int main(){
+	
+	int counter = 1, number, largest = 0 ;
+
+	while ( counter <= 10 ) {
+		printf("Enter non-negeative integer ( %d ): " , counter );
+		scanf("%d", &number);
+	
+		largest = ( number > largest ) ? number : largest ; 
+		counter++ ; 
+	}
+
+	printf("\nLargest of 10 numbers is %d", largest );
+	return 0 ;
+}
+
