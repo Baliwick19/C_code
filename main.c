b2210dce81a0f40ea2217db19862a1943ba6d955
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-01 12:17:36 +0530

    tabular_output

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 // p3_24  tabular_output
+
+#include <stdio.h>
+
+int main(){
+	
+	int n = 1 ; 
+	
+	printf("\n\nN \t N^2 \t N^3 \t N^4 \n\n");
+	while ( n <= 10 ) {
+		printf("%d \t %d \t %d \t %d\n", n, n*n, n*n*n, n*n*n*n );
+        n++ ; 
+	}
+	return 0 ;
+}
