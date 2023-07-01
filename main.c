d6df23c3ddcb6d770388cb9d8ee8f1b59824e82b
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-01 12:23:43 +0530

    tabular_oputput

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
 // p3_25  tabular_output
+
+#include <stdio.h>
+
+int main(){
+	
+	int n = 1 ; 
+	
+	printf("\n\nN \t N+3 \t N+6 \t N+9 \n\n");
+	while ( n <= 5 ) {
+		int m = 7 * n ; 
+		printf("%d \t %d \t %d \t %d\n", m, m + 3, m + 6, m + 9);
+        n++ ; 
+	}
+	return 0 ;
+}
