bb8e296dee3dc8ac7fca9553942b67f5cfda3535
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-01 15:15:43 +0530

    validating_user_input

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
 // p3_27  validating_user_input
+
+#include <stdio.h>
+
+int main(void) {
+	int student = 1, passes = 0, failures = 0 ; 
+
+	while (student <= 10) {
+		printf("%s", "Enter result (1=pass,2=fail): ");
+		int result = 0;
+		scanf("%d", &result);
+
+		if (result == 1) { 
+			passes++;
+		}
+		else { 
+			failures++ ;
+		}
+
+		student++;
+	}
+
+	printf("Passed %d\n", passes);
+	printf("Failed %d\n", failures);
+
+	if (passes > 8) {
+		puts("Bonus to instructor!");
+	}
+}
