723f00ee585b69625cd9ef994950501a5708049f
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-02 16:35:31 +0530

    armstrong_numbers

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
 /* p3_36 armstrong_numbers */ 
+
+#include <stdio.h>
+
+int main(){
+
+	int num ; 
+
+	printf("Enter an integer: ");
+	scanf("%d", &num);
+
+	int temp = num, sum = 0  ; 
+
+	while ( temp != 0 ) {
+		int digit = temp % 10 ; 
+		sum += digit*digit*digit ; 
+		temp /= 10 ; 
+	}
+
+	if ( sum == num ) {
+		printf("%d is an armstrong number", num);
+	}
+	else {
+		printf("%d is not an armstrong number", num);
+	}
+
+	return 0 ;
+}
