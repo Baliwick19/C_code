536a0229ca6347b8f43da9582c741bbb938426e6
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-02 17:50:33 +0530

    circle

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
 /* p3_41 circle */ 
+
+
+#include <stdio.h>
+
+int main(){
+	
+	double PI = 3.14159, radius ; 
+
+	printf("Enter the radius: ");
+	scanf("%lf", &radius ); 
+
+	printf("Diameter :  %.2lf\n",2 * radius ); 
+	printf("Circumference : %.2lf\n", 2 * PI * radius );
+	printf("Area : %.2lf", PI * radius * radius );
+
+	
+}
