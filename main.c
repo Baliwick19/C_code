65da56c7f0bdd7f128c870b55f4171cfc087f5a7
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-02 14:42:52 +0530

    flyod_triangle

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
 /* p3_34 flyod_traingle */ 
+
+#include <stdio.h>
+
+int main(){
+	
+	int size = 0 ; 
+	
+	printf("Enter the size : ");
+	scanf("%d", &size);
+
+	int i = 1, num = 1  ; 
+	while ( i <= size ) {
+		int j = 1 ;
+		while ( j <= i ) {
+			printf("%3d ", num );
+			num++ ;
+			j++ ; 
+		}
+		printf("\n");
+		i++ ;
+	}
+
+	return 0 ;
+}
