34855b429c3d9b1cf6252fb147767a4cf8fc16ac
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-02 18:08:10 +0530

    sides_of_a_triangle

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 /* p3_44 sides_of_a_triangle */ 
+
+#include <stdio.h>
+
+int main(){
+	
+	int side1, side2, side3 ; 
+
+	printf("Enter sides of a triangle : ");
+	scanf("%d %d %d", &side1, &side2, &side3);
+
+	printf(( side1 + side2 > side3 ) ? "Valid sides" : "Invalid sides");
+
+	return 0 ;
+}
