0c6d4d0d620ead84b12a0dc3347e65ff41a8d5c1
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-02 21:55:54 +0530

    world_populaiton_growth

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
 /* p3_46 world_population_growth */ 
+
+#include <stdio.h>
+
+int main(){
+	
+	double growth_rate ; 
+	int current_year ; 
+	unsigned long long int population ; 
+
+	printf("Enter current year, growth rate and current population : "); 
+	scanf("%d %lf %lld", &current_year, &growth_rate, &population);
+	unsigned long long int population_change = 0 ; 
+		
+	printf("%5s %15s %20s\n\n", "Year", "Population", "Population change");
+
+	int i = 0 ; 
+	while ( i <= 100 ) {
+		unsigned long long int temp = population ; 
+		printf("%5d  %15lld  %20lld\n", current_year + i, population, population_change);
+		population *= growth_rate ;
+		population_change = population - temp ; 
+		i++ ; 
+	}
+
+}
