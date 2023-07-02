9fd46fbf5bf1b74691773b873b55b56723ba58b2
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-02 17:37:43 +0530

    checkboard_patterns

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
 /* p3_39 checkboard_pattern */ 
+
+#include <stdio.h>
+
+int main(){
+	
+	int i = 1 ; 
+	while ( i <= 8 ){
+		
+		printf( ( i % 2 == 0 ) ? " " : "" );
+		int j = 1 ; 
+		while ( j <= 8 ){
+			printf("%s", "* ");
+			j++ ; 
+		}
+		i++ ; 
+		puts("");
+	}
+}
