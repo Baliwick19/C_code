df70f7e2b116f4d002b5e89154e40b85cbcc7ce7
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-02 14:16:53 +0530

    hollow_sqaure_of_asterisks

--- main.c
+++ main.c
@@ -1,1 +1,45 @@
 /* p3_33 hollow_square_of_asterisks */ 
+
+#include <stdio.h>
+
+int main(){
+
+	int size = 0 ; 
+
+	printf("Enter the size : ");
+	scanf("%d", &size);
+	int i = 1 ; 
+
+	while ( i <= size ) {
+		int j = 1 ;
+
+		if ( i == 1 ) {
+
+			while ( j <= size ) {
+
+				printf("*");
+				j++ ; 
+			}
+		}
+		else if ( i == size ) {
+
+			while ( j <= size ) {
+
+				printf("*");
+				j++ ; 
+			}
+		}
+		else {
+			printf("*");
+			j = 2 ; 
+			while ( j < size ) {
+				printf(" ");
+				j++ ; 
+			}
+			printf("*");
+		}
+		printf("\n");
+
+		i++ ; 
+	}
+}
