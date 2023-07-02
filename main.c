878cddfaa5e9b9c711636f179230317a629b84e2
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-02 17:00:54 +0530

    multiples_of_a_number

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 /* p3_37 multiples_of_number */ 
+
+#include <stdio.h>
+
+int main(){
+	
+	int i = 1 ; 
+
+	while ( i <= 500 ) {
+		printf(( i % 50 == 0 ) ? "$\n" : "$");
+		i++ ; 
+	}
+	return 0 ;
+}
+
