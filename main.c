6e88b7099d586939ea2ed574495b27d366def64f
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-02 17:58:00 +0530

    errors

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
-/* p3_43  */ 
+/* p3_43 errors  */ 
+
+
+/*
+	=> printf("%d", --(x * y)); ; 
+	
+	increment or decremrnt operator cannot be used on expressions. 
+	
+	->  int z = x * y ; 
+		printf("%d", --(z)) ;
+
+	therfore, we must create a another variable to store value of x * y. 
+	
+*/
+
+
