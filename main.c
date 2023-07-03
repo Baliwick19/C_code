c670a4ef073ebb2a4625db439e4196d8bb0a74a5
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-03 20:33:01 +0530

    errors

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
 /* p4_4 errors */ 
+
+/*
+
+	a) semicolon after while condition 
+	b) representational error. ( comparing float values may lead to error ) 
+	c) break statment after case 1 statment 
+	d) it will print 1 to 9. not 1 to 10 ( off by one error ) .
+
+*/
