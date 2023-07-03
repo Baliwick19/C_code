fd31d95ff81185210c09f9d9fe9bf70a7f7d818c
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-03 20:23:52 +0530

    c_statements

--- main.c
+++ main.c
@@ -1,1 +1,35 @@
 /* p4_3 c_statements */ 
+
+//  a) 
+//  int sum = 0 ; 
+//  for ( int count = 1 ; count <= 99 ; count++ ) {
+//			sum += count ; 
+//  }
+//
+//  b) 
+//  printf("%-15.1f", 333.546372) ; 
+//  printf("%-15.2f", 333.546372) ; 
+//  printf("%-15.3f", 333.546372) ; 
+//  printf("%-15.4f", 333.546372) ; 
+//  printf("%-15.5f", 333.546372) ; 
+//
+//  c) 
+//  printf("%10.2f", pow(2.5,3) ; 
+//
+//  d)
+//    int counter = 1 ; 
+//    while ( counter <= 20 ){
+//      printf("%4d ", counter) ; 
+//      if ( counter%5 == 0 ){
+//			printf("\n") ;
+//     }
+//     counter++ ; 
+//    }
+//
+//  e) 
+//  for ( int counter = 1 ; counter <= 20 ; i++ ) {
+//		printf("%4d ", counter);
+//		if ( counter%5 == 0 ){
+//			printf("\n") ;
+//		}
+//  }
