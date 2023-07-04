9f601b9e1a89614aad6889c801f9dc8ce85d03b7
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-04 13:59:29 +0530

    converting_celcius_to_fahrenite

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
 /* p4_10 converting_celcius_to_fahrenite */ 
+
+
+#include <stdio.h>
+
+int main(){
+	
+	int celcius = 0 ; 
+	printf("%10s %10s\n", "Celcius" , "Fahrenite");
+	for ( celcius = 30 ;  celcius <= 50; celcius++ ){
+		printf("%10d %10.2lf\n", celcius, (9.0/5)*celcius + 32 ); 
+	}
+	return 0 ;
+}
