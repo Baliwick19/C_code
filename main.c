5d7dcd4a332b9a907a547777addf37c21c24d917
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-04 13:45:27 +0530

    sum_and_average_of_integers

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
-/* p4_9  */ 
+/* p4_9	sum_and_average_of_integers  */ 
+
+
+#include <math.h>
+#include <stdio.h>
+
+int main(){
+	
+	int number_of_data = 0 ; 
+
+	printf("Enter the number of data: "); 
+	scanf("%d", &number_of_data); 
+	int i = 1 ; 
+	int sum = 0 ; 
+	int data = 0 ; 
+	while ( i <= number_of_data ) {
+		printf("Enter the data (%d): ", i);
+		scanf("%d", &data); 
+		sum += data ; 
+		i++ ; 
+	}
+
+	printf("\nSum of %d given numbers: %d\n", number_of_data, sum );
+	printf("Average : %.2lf", (double) sum / number_of_data );
+
+	return 0 ; 
+
+}
