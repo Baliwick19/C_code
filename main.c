40ff1c4810a35c6f703744a1fd2fb1721beea65b
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-04 15:46:11 +0530

    checking_if_a_number_is_prime

--- main.c
+++ main.c
@@ -3,11 +3,31 @@
 #include <stdio.h>
 
 int main(){
-	
-	int num ; 
+
+	unsigned long long int num ; 
 
 	printf("Enter a number: "); 
-	scanf("%d", &num ); 
-	
+	scanf("%lld", &num ); 
 
+	int flag = 1 ; 
+
+	unsigned long long int i = 2 ; 
+	if ( num == 1 ) flag = 0 ; 
+	else 
+	while ( i <= num/2.0 ) {
+		if ( num % i == 0 ){
+			flag = 0 ; 
+			i = num ; 
+		}
+		i++   ; 
+	};
+
+	if ( flag == 1 ) {
+		printf("Prime");
+	}
+	else {
+		printf("Not Prime");
+	}
+
+	return 0 ;
 }
