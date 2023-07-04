23b27a8353a53eb7f6fcff451dcb4d429b8eb4cc
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-04 19:41:02 +0530

    factorials

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
 /* p4_14 factorials */ 
+
+#include <stdio.h>
+
+int main(){
+
+	unsigned long long factorial = 1 ; 
+
+	printf("%10s %22s\n", "Integer", "Factorial" );
+	for ( int j = 0 ; j <= 20 ; j++){
+		factorial = 1 ; 
+		for( int i = 1 ; i <= j ;  i++ ) {
+			factorial *= i ; 
+		}
+		printf("%10d! %22lld\n", j, factorial); 
+	}
+	return 0 ;
+}
