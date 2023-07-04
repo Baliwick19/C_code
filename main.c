7aa735597e313faeda1978ac5a0549c4b85f89c3
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-04 14:04:59 +0530

    sum_of_multiples

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 /* p4_11 sum_of_multiples */ 
+
+#include <stdio.h>
+
+int main(){
+	int sum = 0 ; 
+	
+	for ( int i = 7 ; i <= 100 ; i += 7 ){
+		sum += i ; 
+	}
+
+	printf("Sum of all multiples of 7: %d", sum); 
+	return 0 ;
+	
+}
