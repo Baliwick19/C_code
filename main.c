d71992866e7291b296f24ae9c66b9a60730f538a
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-06 14:08:37 +0530

    bar_chart

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
 /* p4_18 bar_chart */ 
+
+#include <stddef.h>
+#include <stdio.h>
+int main(){
+	
+	size_t size = 0 ; 
+
+	for ( size_t i = 1 ; i <= 5 ; i++ ) {
+		printf("Enter number %zu: ", i); 
+		scanf("%zu", &size );
+
+		for ( size_t j = 1 ; j <= size ; j++ ) { 
+			printf("*");
+		}
+		printf("\n\n");
+	}
+}
