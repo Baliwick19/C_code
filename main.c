29390ae1cb28c9c800260079371ace42130e8f32
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-06 21:53:30 +0530

    number_systems

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
 /* p4_25 number_systems */ 
+
+
+#include <stdio.h>
+int main(){
+	
+	printf("%15s%15s%15s%15s\n\n", "Decimal", "Binary", "Octal", "Hexadecimal" );
+	for ( int i = 1 ; i <= 256 ; i++ ) { 
+		
+		printf("%15d%15b%15o%15X\n", i, i, i, i);
+
+	}
+}
