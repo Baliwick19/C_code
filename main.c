20e6a6c0608348393ac22e25503cb6a6076e4793
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-07 14:41:15 +0530

    diamond_printing

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
 /* p4_31 diamond_printing */ 
+
+
+#include <stdio.h>
+#include <wctype.h>
+int main(){
+	
+	int size = 0 ; 
+
+	printf("Enter size: " );
+	scanf("%d", &size );
+	int k = 0 ; 
+	size++ ; 
+	for ( int i = 1 ; i <= size    ;  i++ ) {
+		( i > size / 2 ) ? ( k = size - i ) : ( k = i ) ; 
+		for ( int j = 1 ; j <= size / 2 + k ; j++ ) {
+			printf( ( j <= size / 2  - k + 1   ) ? " " : "*" );
+		}
+		printf("\n");
+		
+	}
+}
