8d617bb945cc14626524e5d5dfcb989d3c664a44
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-07 16:36:22 +0530

    p4_37_remove_continue

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
 /* p4_37 p4_37_remove_continue */ 
+
+#include <stdio.h>
+int main(){
+
+	// replacing continue statement to it equivalent structured statement . 
+	for ( int i = 1 ; i <= 10 ; i++ ) {
+		printf("%d ", i );
+		if ( i == 7 ) i = 8 ; 
+	}
+}
