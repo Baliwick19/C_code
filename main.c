dfe9778de24d4d3cfb1f0d6834f2ad80b9a2d6f2
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-07 10:28:16 +0530

    pythagorean_triples

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
 /* p4_27 pythagorean_triples */ 
+
+
+#include <math.h>
+#include <stdio.h>
+int main(){
+	int n = 1 ;	
+	for ( int i = 1 ; i <= 500 ; i++ ) { 
+		for ( int j = 1 ; j <= sqrt(250000-i*i) && i >= j  ; j ++ ) {
+			for ( int k = 1 ; k <= 500 ; k++ ) {
+				if ( i*i + j*j == k*k ) {
+					printf("%5d   [%4d,%4d,%4d]\n", n, j, i, k);
+					n++ ; 
+				}
+			}
+		}
+	}
+}
