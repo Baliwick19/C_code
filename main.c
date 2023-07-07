92ec0917d60c37cd592378a50fcd39455c47fddf
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-07 16:33:09 +0530

    p4_36_following

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
 /* p4_36 p4_36following */ 
+
+#include <stdio.h>
+
+int main(){
+
+	int n = 4, a = 1; 
+	int i, c;
+	for (i = 1; i <= n; i++) 
+	{
+		for (c = 1; c <= i; c++) 
+		{
+			printf("%d", a);
+			a++ ; 
+		}
+		printf("\n");
+	}
+}
+
+
+/* Output : */ 
+
+/* 1 */
+/* 23 */
+/* 456 */
+/* 78910 */
+
