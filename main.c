8263213f95b0d7e89bf5ceca238b90bb1b2f4007
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-07 16:27:35 +0530

    break_and_continue

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
 /* p4_35 break_and_continue */ 
+
+
+#include <stdio.h>
+int main(){
+
+	// replacing break statement to it equivalent structured statement . 
+	for ( int i = 1 ; i <= 10 ; i++ ) { 
+		printf("%d ", i);
+		if ( i == 7 ) i = 11 ; 
+	}
+
+	printf("\n\n");
+
+	// replacing continue statement to it equivalent structured statement . 
+	for ( int i = 1 ; i <= 10 ; i++ ) {
+		printf("%d ", i );
+		if ( i == 7 ) i = 8 ; 
+	}
+}
