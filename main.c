a6b9ce120dfdfcc760ccaf966887ca522c76c4b6
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-08 21:12:19 +0530

    p5_1_fib

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
 /* p5_1 p5_1_fib */ 
+
+
+/*
+
+	a) function 
+	b) function-call 
+	c) local variable 
+	d) return 
+	e) void 
+	f) scope 
+	g) return; or return expression; or encountering the closing right brace of a function
+	h) function prototype 
+	i) rand
+	j) srand 
+	k) auto; register; extern. static 
+	l) auto 
+	m) global, external
+	n) static 
+	o) block; file; function prototype; function 
+	p) recursive 
+	q) base	
+
+*/
