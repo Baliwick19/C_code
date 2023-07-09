af9a78ca1ffb1380cbed1a16fde827f10dc56d7e
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-09 13:24:39 +0530

    p5_4_function_header

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
 /* p5_4 p5_4_function_header */ 
+
+// a) 
+
+// double hypotenuse ( double side1, double side2)   
+
+// b) 
+
+// int smallest( int x, int y, int z )  
+
+// c) 
+
+// void instructions( void ) 
+
+// d) 
+
+// float intToFloat ( int number )  
+
+
