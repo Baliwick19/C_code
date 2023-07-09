09970c916f69ff8b0d1055ef676e17aaa0cbefd4
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-09 13:27:04 +0530

    p5_5_function_prototype

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
 /* p5_5 p5_5_function_prototype */ 
+
+// a) 
+
+double hypotenuse ( double, double ) ; 
+
+// b) 
+
+int smallest( int, int, int ) ; 
+
+// c) 
+
+void instructions( void ) ;  
+
+// d) 
+
+float intToFloat ( int ) ; 
