6721c24db68f298472aff6f7185d58e62823a887
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-09 13:38:23 +0530

    p5_7_error

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
 /* p5_7 p5_7_error */ 
+
+// a)  function shouldn't be nested. write them individually. 
+
+// b) not value returned. add return result ; 
+
+// c) float a is defined in the argument and a semicolon after parenthesis. remove declaration of float a from function block . 
+
+// d) assigment to n must be done in else block, i.e. n += sum( n - 1 ) ;  
+
+// e) remove return statement. since return type is void. 
