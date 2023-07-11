fa30481c5ebb258a79a59b16156e8e4dedf92f05
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-11 17:45:46 +0530

    p5_22_seperating_digits

--- main.c
+++ main.c
@@ -1,1 +1,39 @@
 /* p5_22 p5_22_seperating_digits */ 
+
+
+#include <math.h>
+#include <stdio.h>
+
+void seperating_digits ( int )  ; 
+
+void seperating_digits ( int num ) { 
+	int size = 0 ; 
+
+	int temp = num ; 
+
+	while ( temp != 0 ) { 
+		temp /= 10 ;
+		size++ ; 
+	}
+
+	temp = num ; 
+
+	for ( int i = pow(10, size-1) ; i >= 1 ; i /= 10 ) {
+		printf("%d ", ( temp / i  ) % 10 );
+	}
+	printf("\n");
+}
+
+
+int main(){
+
+
+	int num ; 
+
+	printf("Enter a integer : ");
+	scanf("%d", &num);
+
+	seperating_digits( num );
+	
+	return 0 ;
+}
