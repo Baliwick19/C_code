222b0e033bd75d1559eda8218a314709ae2569e3
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-11 18:38:29 +0530

    p5_25_maximum

--- main.c
+++ main.c
@@ -1,1 +1,32 @@
 /* p5_25 p5_25_maximum */ 
+
+
+#include <stdio.h>
+
+float largest ( void ) { 
+	
+	int i = 1 ; 
+
+	float num,  largest ; 
+
+	while ( i < 5 ) { 
+		
+		printf("Enter number %d : ", i );
+		scanf("%f", &num);
+
+		largest = ( i == 1 ) ? num : ( ( largest < num ) ? num : largest ) ;
+
+		i++ ; 
+
+	}
+
+	return largest;
+}
+
+int main(){
+	
+	printf("%.2f\n", largest( ));
+
+	return 0 ;
+}
+
