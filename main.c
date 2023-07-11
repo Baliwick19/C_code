a70a6da0bb9cbfb7eafef1bd702f1f23d5396b0d
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-11 11:01:32 +0530

    p5_10_rounding_numbers

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
 /* p5_10 p5_10_rounding_numbers */ 
+
+
+
+
+#include <math.h>
+#include <stdio.h>
+int main(){
+	
+	float x ; 
+
+	printf("Enter a number: "); 
+	scanf("%f",&x); 
+
+	printf("Rounded values of %.2f is %.1f\n", x, floor( x + 0.5)) ;
+
+	return  0 ;
+}
