a0ec81efaa30c3649bac8eab41d8abc339a1ac84
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-11 12:58:25 +0530

    p5_18_even_odd

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
 /* p5_18 p5_18_even_odd */ 
+
+
+
+#include <stdio.h>
+
+int even_odd ( int ) ; 
+
+int even_odd ( int a ){
+	if ( a % 2 == 0 )
+		return  1 ;
+	else
+		return 0 ;
+}
+
+int main(){
+	int num ; 
+
+	printf("Enter an interger : ");
+	scanf("%d", &num); ;
+
+	printf("Status : \n%d\n", even_odd( num ));
+
+	return  0 ;
+}
