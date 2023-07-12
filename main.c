79ca135b1ebc88636492e7761125e1f3009ae84b
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-12 15:14:21 +0530

    p5_28_sum_of_digits

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
 /* p5_28 p5_28_sum_of_digits */ 
+
+#include <stdio.h>
+
+int sum_of_digits ( unsigned long long int num ) { 
+	int sum = 0 ; 
+
+	while ( num != 0 ) {
+		sum += num % 10 ; 
+		num /= 10 ; 
+	}
+	return sum ;
+}
+
+int main(){
+	
+	unsigned long long int num ; 
+
+	printf("Enter a a non negative integer : ");
+	scanf("%lld", &num );
+
+	printf("\n\nSum: %3d\n\n", sum_of_digits( num ));
+
+	return 0 ;
+
+}
