12dc60b3383d1f05c186fd44d1f211cd8384df3c
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-12 19:24:51 +0530

    p5_30_quality_points

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
 /* p5_30 p5_30_quality_points */ 
+
+
+#include <stdio.h>
+
+int qualityPoints ( double ) ; 
+
+int qualityPoints ( double average ) {
+	int point = 0 ; 
+
+	if ( average >= 90 ) point = 4 ; 
+	else if ( average >= 80 ) point = 3 ;
+	else if ( average >= 70 ) point = 2 ; 
+	else if ( average >= 60 ) point = 1 ; 
+	else point = 0 ;
+
+	return  point ;
+}
+
+int main(){
+	double average = 0.0 ; 
+
+	printf("Enter the average : ");
+	scanf("%lf", &average);
+
+	printf("\nPoints : %d\n", qualityPoints( average ));
+
+	return  0 ;
+}
