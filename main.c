543063f8e4a2694d15ccc8fb58fd61631e1b7791
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-15 21:50:17 +0530

    p6_9_statements

--- main.c
+++ main.c
@@ -1,1 +1,39 @@
 /* p6_9 p6_9_statements */ 
+
+/*
+
+	a) int grades[5][20] ; 
+	b) 5 
+	c) 20 
+	d) 100 
+	e) grades[i][0] ; where   1 <= i <= 4 
+	f) grades[2][1] 
+	g) grades[0][1] = 100 ; 
+	h) for ( size_t i = 0 ; i < 5 ; i++ ){
+			for ( size_t j = 0 ; j < 20 ; j++){
+				scanf("%d", grades[i][[j]) ; 
+			}
+		}
+	i) for ( size_t i = 0 ; i < 5 ; i++ ){
+			for ( size_t j = 0 ; j < 20 ; j++){
+				grades[i]][j] = 0 ; 
+			}
+		}
+	j) for ( size_t i = 0 ; i < 20 ; i++ ){
+			marks[0][i] = mathGrades[i]	; 
+		}
+	k) int highest = grades[0][0] ; 
+		for ( size_t i == 0 ; i < 20 ; i++ ) {
+			highest = ( highest < grades[0][i] ) ? grades[0][i] : highest ; 
+		}
+	l) for ( size_t i = 0 ; i < 5 ; i++ ) {
+			printf("%d ", grades[i][1] ) ; 
+		}
+	m) double average = 0 ; 
+		for ( size_t i = 0 ; i < 20 ; i++ ) {
+			average += (double) i / 20 ; 
+			}
+	n) 
+ 
+
+*/
