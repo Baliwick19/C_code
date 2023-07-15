d4b589e5dad91302efa4e01ad2ce49f53275d70e
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-15 20:06:28 +0530

    p6_3_statements

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
 /* p6_3 p6_3_statements */ 
+
+
+/*
+
+	a) #define SIZE = 10 
+	b) double fractions[ SIZE ] = {0.0} ; 
+	c) fractions[4] 
+	d) fractions[9] = 1.667
+	e) fractions[6] = 3.333 
+	f) printf("fractions[6] = %.2lf and fractions[9] = %.2lf.", fractions[6], fractions[9])
+	g) for ( size_t x = 0 ; x < SIZE ; x++ ) {
+			printf("fractions[ %zu ] = %lf\n",x ,fractions[x] ) ; 
+		}
+
+*/
