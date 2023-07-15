5f75c1d0df13f08906df69490b1c6cfa8b998758
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-15 20:43:54 +0530

    p6_5_errors

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 	a) semicolon after 100 should be removed.
 	b) SIZE is not a variable. 
 	c) accesing element out of bounds using index 10, ( i < 10 ). 
-	d) semicolon after preprocessor directive 
+	d) semicolon after preprocessor directive. 
 	e) comma seperation of index, should be a[1][1] = 5 ;
 	f) VALUE is not a variable such that we can assign a value to it. equality should be removed. 
 
