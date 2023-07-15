97db5a851c55a3dd0fef2f1c945fbc6eb2bd2b0e
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-15 20:14:48 +0530

    p6_4_statements

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
 /* p6_4 p6_4_statements */ 
+
+/*
+
+	a) int table [SIZE][SIZE] ;  
+	b) 9, printf("Total number of elements = %d", SIZE * SIZE)
+	c) for ( size_t i = 0 ; i <= SIZE ; i++) {
+			for ( size_t j = 0 ; j <= SIZE ; j++) {
+				table[i][j] = i + j ; 
+			}
+	}
+	d) 1 8 0 
+	   2 4 6 
+		5 0 0 
+
+*/
