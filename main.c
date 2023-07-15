b9829f19287aec8a7eadfc0150eddea9238b1f8d
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-15 20:40:56 +0530

    p6_6_fib

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
 /* p6_6 p6_6_fib */ 
+
+/*
+
+	a) static 
+	b) contiguous 
+	c) 0th; array_name[0]
+	d) array_name; index or subscript  
+	e) reference 
+	f) bound 
+	g) assignment 
+	h) null 
+	i) 5; 7; 35 
+	j) a[2][3]
+
+*/
