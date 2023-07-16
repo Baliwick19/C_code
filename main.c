e031dc4dd02c1ad372b793ebc0aa1cbd39148f24
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-16 15:17:21 +0530

    p6_10_sales_commission

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
 /* p6_10 p6_10_sales_commission */ 
+
+
+#include <stdio.h>
+
+int main(){
+	int j = 0 ; 
+	static int k = 0 ; 
+	for ( int i = 100 ; i < 200 ; i++ ) {
+		double per = 0.09 ; 
+		while ( (int) ( per * k )  == i ) { j++; k++ ;  } 
+		printf("%d", k );
+	}
+
+	printf("%d %d\n", j , k );
+}
