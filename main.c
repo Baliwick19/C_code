c1a3e951d384d450c1c996d2d75c4b710db5986f
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-17 17:48:34 +0530

    p6_13_errors

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 /* p6_13 p6_13_errors */ 
+
+
+/*
+                                                             v  v
+a) missing ampersand and out of bound reference: scanf("%d",  a[5]) ; 
+																						   v
+b) out of bound reference: printf("%d%d%d\n", a[0][1], a[0][2], a[0][3]);
+																 v      v      	
+c) semicolon should be comma:  double f[3] = {1.1; 10.01; 100.001, }; => double f[3] = {1.1, 10.01, 100.001, };
+																	 v
+d) index should be written in diffent brackets : d[2, 4] = 2.345 => d[2][4] = 2.345
+*/
+
+
