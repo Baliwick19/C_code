f77841b64adafd542d86b8063e9f1f1412682297
Baliwick19 <sutharbhavesh121981@gmail.com>
2023-07-18 16:27:03 +0530

    p6_16_label

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
 /* p6_16 p6_16_label */ 
+
+#include <stdio.h>
+
+int main(){
+
+	int sales[3][5] ;
+
+	for (size_t row = 0; row <= 2; ++row) {
+		for (size_t column = 0; column <= 4; ++column) {
+			sales[row][column] = 0;
+			printf("sales[%zu][%zu] ", row, column) ; 
+		}
+		puts("");
+	}
+
+}
